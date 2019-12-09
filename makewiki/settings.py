63509fc852b4e5f51f3d2f674ff1fa7f7b452bdc
omarsagoo <om.sagoo@gmail.com>
2019-12-09 08:00:52 -0800

    commit changes

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'make-wiki-v2-123.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'sagoomakewikiv2.herokuapp.com']
 
 
 # Application definition
